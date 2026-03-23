86b3c675d1b87b89fed8bc9b6b6d02f6e7564103
Tesla <tesla.a.lyon@gmail.com>
2026-03-22 23:46:49 -0700

    Fixed import errors in routes/auth.js and middleware/auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
-import User from "habit-tracker/models/User.js";
-import { protect } from "../middleware/auth";
+import User from "../models/User.js";
+import { protect } from "../middleware/auth.js";
 import jwt from "jsonwebtoken";
 
 
