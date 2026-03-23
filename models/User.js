c6b6f3dc5436c92ac82146bbf3a206f116354628
Tesla <tesla.a.lyon@gmail.com>
2026-03-22 22:39:56 -0700

    Added User model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+userSchema.pre("save", async function(next){
+    // if password is modified (aka salted and hashed) then skip salt step
+    if (!this.isModified("password")) next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+// method to check if entered password for login matches user's correct password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
